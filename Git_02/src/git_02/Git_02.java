16b9aac688da1b0e6bebcd201e0061c4fad13e4d
USER <USER@Frank>
2018-01-16 14:48:27 -0500

    Valentin_02

---
+++ Git_02/src/git_02/Git_02.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_02;
+
+/**
+ *
+ * @author USER
+ */
+public class Git_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    public void mostrar_valentin(){
+        System.out.println("123");
+    }
+    
+}
